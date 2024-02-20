99f7a3d84edd25403687cc248ded7f629acfc434
le2yeji <leyagi17@gmail.com>
2024-02-20 18:33:13 +0900

    init commit

---
+++ src/main/java/com/kt/edu/thirdproject/common/runner/KeycloakInitializerRunner.java
@@ -0,0 +1,107 @@
+package com.kt.edu.thirdproject.common.runner;
+
+import com.kt.edu.thirdproject.common.config.WebSecurityConfig;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.representations.idm.ClientRepresentation;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.RealmRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class KeycloakInitializerRunner implements CommandLineRunner {
+
+    private final Keycloak keycloakAdmin;
+
+    @Override
+    public void run(String... args) {
+        log.info("Initializing '{}' realm in Keycloak ...", EMPLOYEE_SERVICE_REALM_NAME);
+
+        Optional<RealmRepresentation> representationOptional = keycloakAdmin.realms()
+                .findAll()
+                .stream()
+                .filter(r -> r.getRealm().equals(EMPLOYEE_SERVICE_REALM_NAME))
+                .findAny();
+        if (representationOptional.isPresent()) {
+            log.info("Removing already pre-configured '{}' realm", EMPLOYEE_SERVICE_REALM_NAME);
+            keycloakAdmin.realm(EMPLOYEE_SERVICE_REALM_NAME).remove();
+        }
+
+        // Realm
+        RealmRepresentation realmRepresentation = new RealmRepresentation();
+        realmRepresentation.setRealm(EMPLOYEE_SERVICE_REALM_NAME);
+        realmRepresentation.setEnabled(true);
+        realmRepresentation.setRegistrationAllowed(true);
+
+        // Client
+        ClientRepresentation clientRepresentation = new ClientRepresentation();
+        clientRepresentation.setClientId(EDU_APP_CLIENT_ID);
+        clientRepresentation.setDirectAccessGrantsEnabled(true);
+        clientRepresentation.setPublicClient(true);
+        clientRepresentation.setRedirectUris(List.of(EDU_APP_REDIRECT_URL));
+        clientRepresentation.setDefaultRoles(new String[]{WebSecurityConfig.EDU_USER});
+        realmRepresentation.setClients(List.of(clientRepresentation));
+
+        // Users
+        List<UserRepresentation> userRepresentations = EDU_USER_LIST.stream()
+                .map(userPass -> {
+                    // User Credentials
+                    CredentialRepresentation credentialRepresentation = new CredentialRepresentation();
+                    credentialRepresentation.setType(CredentialRepresentation.PASSWORD);
+                    credentialRepresentation.setValue(userPass.password());
+
+                    // User
+                    UserRepresentation userRepresentation = new UserRepresentation();
+                    userRepresentation.setUsername(userPass.username());
+                    userRepresentation.setEnabled(true);
+                    userRepresentation.setCredentials(List.of(credentialRepresentation));
+                    userRepresentation.setClientRoles(getClientRoles(userPass));
+
+                    return userRepresentation;
+                })
+                .toList();
+        realmRepresentation.setUsers(userRepresentations);
+
+        // Create Realm
+        keycloakAdmin.realms().create(realmRepresentation);
+
+        // Testing
+        UserPass admin = EDU_USER_LIST.get(0);
+        log.info("Testing getting token for '{}' ...", admin.username());
+
+        Keycloak keycloakMovieApp = KeycloakBuilder.builder().serverUrl(KEYCLOAK_SERVER_URL)
+                .realm(EMPLOYEE_SERVICE_REALM_NAME).username(admin.username()).password(admin.password())
+                .clientId(EDU_APP_CLIENT_ID).build();
+
+        log.info("'{}' token: {}", admin.username(), keycloakMovieApp.tokenManager().grantToken().getToken());
+        log.info("'{}' initialization completed successfully!", EMPLOYEE_SERVICE_REALM_NAME);
+    }
+
+    private Map<String, List<String>> getClientRoles(UserPass userPass) {
+        List<String> roles = new ArrayList<>();
+        roles.add(WebSecurityConfig.EDU_USER);
+        if ("admin".equals(userPass.username())) {
+            roles.add(WebSecurityConfig.EDU_MANAGER);
+        }
+        return Map.of(EDU_APP_CLIENT_ID, roles);
+    }
+
+    private static final String KEYCLOAK_SERVER_URL = "http://localhost:8080";
+    private static final String EMPLOYEE_SERVICE_REALM_NAME = "employee-service";
+    private static final String EDU_APP_CLIENT_ID = "edu-app";
+    private static final String EDU_APP_REDIRECT_URL = "http://localhost:3000/*";
+    private static final List<UserPass> EDU_USER_LIST = Arrays.asList(
+            new UserPass("admin", "admin"),
+            new UserPass("user", "user"));
+
+    private record UserPass(String username, String password) {
+    }
+}
