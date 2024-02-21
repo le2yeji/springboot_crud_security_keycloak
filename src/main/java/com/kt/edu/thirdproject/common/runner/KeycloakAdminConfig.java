d6be1a874d4e14cb9b0f3959d62b9933bc0b1754
le2yeji <96720279+le2yeji@users.noreply.github.com>
2024-02-21 09:51:36 +0900

    Update KeycloakAdminConfig.java

--- src/main/java/com/kt/edu/thirdproject/common/runner/KeycloakAdminConfig.java
+++ src/main/java/com/kt/edu/thirdproject/common/runner/KeycloakAdminConfig.java
@@ -14,7 +14,7 @@ public class KeycloakAdminConfig {
                 .serverUrl("http://211.43.12.238:30777")
                 .realm("master")
                 .username("admin")
-                .password("admin")
+                .password("New1234!")
                 .clientId("admin-cli")
                 .build();
     }
